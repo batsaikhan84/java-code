278eb12722b379aff8fed1485f599a3b0fb773c0
batsaikhan ulambayar <u.batsaikhan@gmail.com>
2020-11-05 21:48:59 -0600

    practice at if statements

--- src/main/java/WeekOne/controlFlow/AboutIf_Else.java
+++ src/main/java/WeekOne/controlFlow/AboutIf_Else.java
@@ -1,5 +1,7 @@
 package WeekOne.controlFlow;
 
+import java.util.Random;
+
 public class AboutIf_Else {
         /*
     1. Only if statement
@@ -11,6 +13,29 @@ public class AboutIf_Else {
         int num = 5;
         if (num >= 5) {
             System.out.println("It's valid number");
+        }
+        if (num % 2 != 0) {
+            System.out.println("It's an odd number");
+        } else {
+            System.out.println("It's an even number");
+        }
+        if (num <= 4) {
+            System.out.println("Number is within 0 to 4");
+        } else if (num <= 10) {
+            System.out.println("Number is within 5 to 10");
+        } else {
+            System.out.println("It's not valid number");
+        }
+        Random random = new Random();
+        int a = random.nextInt(5);
+        if (a == 1) {
+            System.out.println("It's one");
+        } else if (a == 2) {
+            System.out.println("It's two");
+        } else if (a == 3) {
+            System.out.println("It's three");
+        } else {
+            System.out.println("Not valid number");
         }
     }
 }
