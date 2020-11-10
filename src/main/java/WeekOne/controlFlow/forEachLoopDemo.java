dd4c6c10af6d6a71588a27a7f38ae7765366dbb6
batsaikhan ulambayar <u.batsaikhan@gmail.com>
2020-11-09 18:48:01 -0600

    For each loop demo

---
+++ src/main/java/WeekOne/controlFlow/forEachLoopDemo.java
@@ -0,0 +1,13 @@
+package WeekOne.controlFlow;
+
+public class forEachLoopDemo {
+//    for (int i: nameOfArray) {
+//        .. code ..
+//    }
+    public static void main(String[] args) {
+        double [] arr = {12.99, 23.99, 45.99, 35.78};
+        for (double i: arr) {
+            System.out.println(i);
+        }
+    }
+}
