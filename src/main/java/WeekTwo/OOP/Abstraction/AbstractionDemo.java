71b4b24d31c10b3e7633586a1178065be9ed7ed5
batsaikhan ulambayar <u.batsaikhan@gmail.com>
2020-11-17 10:21:26 -0600

    Abstraction demo

--- src/main/java/WeekTwo/OOP/Abstraction/AbstractionDemo.java
+++ src/main/java/WeekTwo/OOP/Abstraction/AbstractionDemo.java
@@ -1,4 +1,20 @@
 package WeekTwo.OOP.Abstraction;
 
-public class AbstractionDemo {
+public class AbstractionDemo extends Meal{
+    public static void main(String[] args) {
+        AbstractionDemo food = new AbstractionDemo();
+        System.out.println(food.prepMeal("Lunch", "Soda"));
+        System.out.println(food.prepMeal("Dinner", "Wine"));
+    }
+
+    @Override
+    public String prepMeal(String mealType, String drink) {
+        String foodType = mealType;
+        String drinkType = drink;
+        return foodType + " " + drinkType;
+    }
+}
+//Class meal
+abstract class Meal {
+    public abstract String prepMeal(String mealType, String drink);
 }
