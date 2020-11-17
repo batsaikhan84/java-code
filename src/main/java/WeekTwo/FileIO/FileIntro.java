99fbc6bf4820b0fce75be856220aa6e2c6b1ddaa
batsaikhan ulambayar <u.batsaikhan@gmail.com>
2020-11-17 11:27:48 -0600

    Complete file intro

--- src/main/java/WeekTwo/FileIO/FileIntro.java
+++ src/main/java/WeekTwo/FileIO/FileIntro.java
@@ -1,9 +1,9 @@
 package WeekTwo.FileIO;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileIntro {
+    //create a file
     static void createFile() {
         File file = new File("Data.txt");
         try {
@@ -18,8 +18,37 @@ public class FileIntro {
             e.getMessage();
         }
     }
+    //write a file
+    static void writeFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Data.txt"))) {
+            String message = "This is file writer";
+            writer.write(message);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //read a file
+    static void readFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("Data.txt"))) {
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                System.out.println(currentLine);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Done reading the file");
+        }
+    }
+
 
     public static void main(String[] args) {
         createFile();
+        writeFile();
+        readFile();
     }
 }
